58377e3a6a7f303f6390be0048464beb890bba5c
Foxodude <apolmaks1@gmail.com>
2024-03-26 22:38:46 +0300

    231

---
+++ UniLab4.py
@@ -0,0 +1,29 @@
+# -*- coding: cp1251 -*-
+#
+import random
+
+def checker1(num):
+    if num % 3 == 0: 
+        print("Число делится на 3")
+    else: 
+        print("Число не делится на 3")
+
+while True:
+    checker = False
+    primaryChoice = input("Выбор задания для проверки : \n1)\n2)\n3)\n4)\n5) - Выход\nВыбор : ")
+    if [num for num in primaryChoice if num not in ".,/*-+1234567890"]: checker = True
+    if checker == True:
+        print("Так нельзя")
+        break
+    primaryChoice = int(primaryChoice)
+    
+    if primaryChoice == 1:
+        print("Введите число для проверки для деления на 3")
+        num = input()
+        num = int(num)
+        checker1(num)
+        
+    if primaryChoice == 2:
+        print("Введите число, которое будет делить сотню")
+        
+        
